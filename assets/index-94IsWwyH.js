0f5e750cba23d895b322c1f599b7d91205f95ed5
ahmedelsawy17 <ahmedalabd782@gmail.com>
2026-05-08 18:24:03 +0300

    Add files via upload

---
+++ assets/index-94IsWwyH.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-DMRRA_aH.js","./index-DzAmrokj.js"])))=>i.map(i=>d[i]);
+import{_ as r}from"./index-Dun2l8e3.js";import{registerPlugin as t}from"./index-DzAmrokj.js";const p=t("SplashScreen",{web:()=>r(()=>import("./web-DMRRA_aH.js"),__vite__mapDeps([0,1]),import.meta.url).then(e=>new e.SplashScreenWeb)});export{p as SplashScreen};
